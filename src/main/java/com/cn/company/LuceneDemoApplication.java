4a5f4a84b6474773b97816da0b65e4452c2d79cd
flybetter <flybetter@163.com>
2018-01-09 08:42:23 +0800

    add spring-boot-starter-web. the project is web app now.

--- src/main/java/com/cn/company/LuceneDemoApplication.java
+++ src/main/java/com/cn/company/LuceneDemoApplication.java
@@ -3,10 +3,13 @@ package com.cn.company;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author wubingyu
+ */
 @SpringBootApplication
 public class LuceneDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LuceneDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LuceneDemoApplication.class, args);
+    }
 }
